653269456a33740c9a6de4d3fd798ad79d91b564
Jowney <jowney123@gmail.com>
2018-04-20 23:39:03 +0800

    author:jowney description:新建包

---
+++ app/src/main/java/com/jowney/openglsample_advance/MainActivity.java
@@ -0,0 +1,12 @@
+package com.jowney.openglsample_advance;
+
+import android.app.Activity;
+
+/**
+ * Creator: Jowney  (~._.~)
+ * Date: 2018/4/20/23:37
+ * Description:
+ */
+
+public class MainActivity extends Activity {
+}
